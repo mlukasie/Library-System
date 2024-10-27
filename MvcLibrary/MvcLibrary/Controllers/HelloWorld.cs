989643030e3442da260a5c549f9b0d5613fa4d51
unknown <you@example.com>
2024-10-27 23:09:07 +0100

    First model and view

---
+++ MvcLibrary/MvcLibrary/Controllers/HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcLibrary.Controllers
+{
+    public class HelloWorld : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome()
+        {
+            return "This is my welcome action...";
+        }
+    }
+}
