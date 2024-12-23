56b5934e5f38f9bee1e7667498b62de3728f7e29
mlukasie <mateuszlu2106@gmail.com>
2024-12-23 17:38:08 +0100

    Initial API project

---
+++ LibraryApi/Data/LibraryDbContext.cs
@@ -0,0 +1,12 @@
+﻿using LibraryApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+public class LibraryDbContext : DbContext
+{
+    public LibraryDbContext(DbContextOptions<LibraryDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Book> Books { get; set; }
+}
